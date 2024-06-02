9c80706b438e89b53eecec42b57f06260e3d309b
Durga Prasad <durgaprasad200014@outlook.com>
2024-06-02 18:38:49 +0530

    added links to youtube Project

--- project-details/src/Components/Project_Info.js
+++ project-details/src/Components/Project_Info.js
@@ -1,22 +1,30 @@
-const ProjectInfo = (props)=>{
-    const {project_title,code_link,repo_link} = props?.projDetails
-    return <ul>
-    <li>
-      <span className="w-[130px] inline-block">Project Title </span>
-      <span>:</span> 
-      <span className="ml-[60px]">{project_title}</span>
-    </li>
-    <li>
-      <span className="w-[130px] inline-block">Repo </span>
-      <span>:</span>
-      <span className="ml-[60px]">{code_link}</span>
-    </li>
-    <li>
-      <span className="w-[130px] inline-block">Demo Link</span>
-      <span>:</span> 
-      <span className="ml-[60px]">{repo_link}</span>
-    </li>
-  </ul>
-}
+import { Link } from "react-router-dom";
 
-export default ProjectInfo
+const ProjectInfo = (props) => {
+  const { project_title, code_link, repo_link } = props?.projDetails;
+  return (
+    <ul>
+      <li>
+        <span className="w-[130px] inline-block">Project Title </span>
+        <span>:</span>
+        <span className="ml-[60px]">{project_title}</span>
+      </li>
+      <li>
+        <span className="w-[130px] inline-block">Repo </span>
+        <span>:</span>
+        <span className="ml-[60px]">
+          <Link to={code_link}>{code_link}</Link>
+        </span>
+      </li>
+      <li>
+        <span className="w-[130px] inline-block">Demo Link</span>
+        <span>:</span>
+        <span className="ml-[60px]">
+          <Link to={repo_link}>{repo_link}</Link>
+        </span>
+      </li>
+    </ul>
+  );
+};
+
+export default ProjectInfo;
